5412bf46603c335d377feebcaa665ebbb34040ff
El Bandido <34052552+EB-TNAP@users.noreply.github.com>
2025-03-04 16:51:35 -0500

    Add files via upload

--- lib/dvb/scan.cpp
+++ lib/dvb/scan.cpp
@@ -674,24 +674,6 @@ RESULT eDVBScan::startFilter()
 	return 0;
 }
 
-// Implementation of the new public methods
-
-int eDVBScan::getScanProgress()
-{
-	return m_scan_progress;
-}
-
-int eDVBScan::getScanProgressTotal()
-{
-	return m_scan_progress_total;
-}
-
-eDVBScan::scanState eDVBScan::getScanState()
-{
-	return m_scan_state;
-}
-
-
 bool eDVBScan::optimizeTuneParameters(ePtr<iDVBFrontendParameters> &feparm)
 {
 	if (!feparm)
